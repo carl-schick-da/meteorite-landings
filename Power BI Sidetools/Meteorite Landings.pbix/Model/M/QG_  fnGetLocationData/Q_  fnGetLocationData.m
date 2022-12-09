a4ab03fc5ddf59c79aeb80089303d11bdb101ec3
Carl Schick <carl.schick.biz@gmail.com>
2022-12-09 13:45:33 -0500

    Draft final report.

--- Power BI Sidetools/Meteorite Landings.pbix/Model/M/QG_  fnGetLocationData/Q_  fnGetLocationData.m
+++ Power BI Sidetools/Meteorite Landings.pbix/Model/M/QG_  fnGetLocationData/Q_  fnGetLocationData.m
@@ -1,7 +1,7 @@
 let
     Source = (locations as text) => let
     #"Get Locations Parameter" = if locations = null then "" else locations,
-    #"Extract Script" = File.Contents("R:\OneDrive\Documents\GitHub\meteorite_landings\meteorite_landings_pbi.py"),
+    #"Extract Script" = File.Contents("R:\OneDrive\Documents\GitHub\meteorite-landings\meteorite_landings_pbi.py"),
     #"Prepend Parameter" = "pbi_args = [" & #"Get Locations Parameter" & "]" & "#(lf)#(lf)" & Text.FromBinary(#"Extract Script"),
     #"Execute Script" = Python.Execute(#"Prepend Parameter"),
     #"Retrieve Dataframe" = #"Execute Script"{[Name="location_info"]}[Value]
--- Power BI Sidetools/Meteorite Landings.pbix/Model/M/Q_  Country Codes.m
+++ Power BI Sidetools/Meteorite Landings.pbix/Model/M/Q_  Country Codes.m
@@ -3,6 +3,12 @@ let
     #"Extracted Table From Html" = Html.Table(Source, {{"Column1", "TABLE.js-csv-data.csv-data.js-file-line-container > * > TR > :nth-child(1)"}, {"Column2", "TABLE.js-csv-data.csv-data.js-file-line-container > * > TR > :nth-child(2)"}, {"Column3", "TABLE.js-csv-data.csv-data.js-file-line-container > * > TR > :nth-child(3)"}, {"Column4", "TABLE.js-csv-data.csv-data.js-file-line-container > * > TR > :nth-child(4)"}, {"Column5", "TABLE.js-csv-data.csv-data.js-file-line-container > * > TR > :nth-child(5)"}, {"Column6", "TABLE.js-csv-data.csv-data.js-file-line-container > * > TR > :nth-child(6)"}, {"Column7", "TABLE.js-csv-data.csv-data.js-file-line-container > * > TR > :nth-child(7)"}}, [RowSelector="TABLE.js-csv-data.csv-data.js-file-line-container > * > TR"]),
     #"Promoted Headers" = Table.PromoteHeaders(#"Extracted Table From Html", [PromoteAllScalars=true]),
     #"Changed Type" = Table.TransformColumnTypes(#"Promoted Headers",{{"", type text}, {"Country", type text}, {"Alpha-2 code", type text}, {"Alpha-3 code", type text}, {"Numeric code", Int64.Type}, {"Latitude (average)", type number}, {"Longitude (average)", type number}}),
-    #"Removed Columns" = Table.RemoveColumns(#"Changed Type",{""})
+    #"Removed Columns" = Table.RemoveColumns(#"Changed Type",{""}),
+    #"Added Index" = Table.AddIndexColumn(#"Removed Columns", "Index", 1, 1, Int64.Type),
+    #"Reordered Columns" = Table.ReorderColumns(#"Added Index",{"Index", "Country", "Alpha-2 code", "Alpha-3 code", "Numeric code", "Latitude (average)", "Longitude (average)"}),
+    #"Sorted for Duplicates" = Table.Buffer(Table.Sort(#"Reordered Columns",{{"Index", Order.Descending}})),
+    #"Removed Duplicates" = Table.Distinct(#"Sorted for Duplicates", {"Alpha-2 code"}),
+    #"Removed Index" = Table.RemoveColumns(#"Removed Duplicates",{"Index"}),
+    #"Sorted Country" = Table.Sort(#"Removed Index",{{"Country", Order.Ascending}})
 in
-    #"Removed Columns"
+    #"Sorted Country"
